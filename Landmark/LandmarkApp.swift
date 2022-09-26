e4096423fbda46d145a6ae0479a607fa8e18e293
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-26 20:46:55 +0300

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Amby on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
